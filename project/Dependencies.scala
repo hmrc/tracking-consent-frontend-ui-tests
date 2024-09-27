9db09b38105cdaf85c47e00cbfb3ee0c0d2d2d0c
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-09-27 14:37:15 +0100

    PLATUI-3143: Dependencies and README updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object Dependencies {
 
   private val playVersion      = "play-30"
-  private val bootstrapVersion = "9.4.0"
+  private val bootstrapVersion = "9.5.0"
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "ui-test-runner"               % "0.37.0"         % Test,
+    "uk.gov.hmrc" %% "ui-test-runner"               % "0.38.0"         % Test,
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test
   )
 
